6e294886be4c1664494c8f7e17514be4bd082337
Ivan <ivan.karyaking@gmail.com>
2020-04-08 11:54:02 +0000

    Updates

---
+++ precache-manifest.3bd5b96161a39e4c942979ba4d57cbff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20646c737fed676cb06476fba7cda8f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f0de19e6aa0893d1958c",
+    "url": "/monsters-rolodex/static/css/main.754b1ae1.chunk.css"
+  },
+  {
+    "revision": "278e06c46167715a93e9",
+    "url": "/monsters-rolodex/static/js/2.a5bba0b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a5bba0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0de19e6aa0893d1958c",
+    "url": "/monsters-rolodex/static/js/main.9afb7fd3.chunk.js"
+  },
+  {
+    "revision": "429e1860ce4c2178cfdd",
+    "url": "/monsters-rolodex/static/js/runtime-main.3fcc104b.js"
+  }
+]);
